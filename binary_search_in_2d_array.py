cf435b496647560c40f48cd22ad441a199578e55
Anit Paul <127428414+Anit-Paul@users.noreply.github.com>
2024-07-27 21:14:14 +0530

    Create binary_search_in_2d_array.py

---
+++ binary_search_in_2d_array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        m=len(matrix)
+        n=len(matrix[0])
+        low=0
+        high=(m*n)-1
+        while(low<=high):
+            mid=(low+high)//2
+            if(matrix[mid/n][mid%n]==target):
+                return True
+            elif(matrix[mid/n][mid%n]>target):
+                high=mid-1
+            else:
+                low=mid+1
+        return False
+        
